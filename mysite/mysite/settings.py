b68a5978b8a39f61bfbb2e04e13485291fe0334b
laijinyan <ljy080829@gmail.com>
2017-04-30 21:47:00 +0800

    wechat app

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'wechat.apps.WechatConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ mysite/wechat/models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class VAccount(models.Model):
+    vName = models.CharField(max_length=30)
+    vAccount = models.CharField(max_length=30)
+    category = models.CharField(max_length=250)
+    area = models.CharField(max_length=30)
+    tags = models.CharField(max_length=250)
+    link = models.CharField(max_length=250)
+    description = models.TextField()
+    qrCode = models.ImageField(upload_to='uploads/qrCodes/')
+
+class Blog(models.Model):
+    account = models.ForeignKey(VAccount)
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    recordTime = models.DateTimeField(auto_now_add=True)
