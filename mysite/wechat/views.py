2dedaadc0b110510e34f34ecab4255abf7d26000
laijinyan <ljy080829@gmail.com>
2017-05-04 21:31:53 +0800

    可以正常的显示 媒体文件（如头像，二维码等）

--- mysite/wechat/views.py
+++ mysite/wechat/views.py
@@ -3,6 +3,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from .models import VAccount
+
 # Create your views here.
 def index(request):
-    return render(request, 'wechat/index.html')
+    accounts = VAccount.objects.all()[:10]
+    return render(request, 'wechat/index.html', {
+        "accounts": accounts,
+    })
